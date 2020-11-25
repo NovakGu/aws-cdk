5e7c7a830fa5650a0af01bde6927b3c63adb5dfd
Sumeet Badyal <badyals@amazon.com>
2020-11-25 11:31:03 -0800

    feat(stepfunctions-tasks): add EKS Create/Delete Fargate Profile to SFN-tasks

--- packages/@aws-cdk/aws-stepfunctions-tasks/lib/index.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/lib/index.ts
@@ -44,3 +44,5 @@ export * from './athena/stop-query-execution';
 export * from './athena/get-query-execution';
 export * from './athena/get-query-results';
 export * from './databrew/start-job-run';
+export * from './eks/create-fargate-profile';
+export * from './eks/delete-fargate-profile';
